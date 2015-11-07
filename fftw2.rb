2efe83b40dd2711c576a5130de9754ad8681f73d
Matthew R. Becker <beckermr@users.noreply.github.com>
2015-11-07 13:15:18 -0800

    stub fftw2

---
+++ fftw2.rb
@@ -0,0 +1,52 @@
+class Fftw < Formula
+  desc "legacy FFTW 2.1.5"
+  homepage "http://www.fftw.org"
+  url "http://www.fftw.org/fftw-2.1.5.tar.gz"
+  
+  option "with-fortran", "Enable Fortran bindings"
+  option :universal
+  option "with-mpi", "Enable MPI parallel transforms"
+  option "with-openmp", "Enable OpenMP parallel transforms"
+
+  depends_on :fortran => :optional
+  depends_on :mpi => [:cc, :optional]
+  needs :openmp if build.with? "openmp"
+
+  def install
+    args = ["--enable-shared",
+            "--disable-debug",
+            "--prefix=#{prefix}",
+            "--enable-threads",
+            "--disable-dependency-tracking"]
+    simd_args = ["--enable-sse2"]
+    simd_args << "--enable-avx" if ENV.compiler == :clang && Hardware::CPU.avx? && !build.bottle?
+
+    args << "--disable-fortran" if build.without? "fortran"
+    args << "--enable-mpi" if build.with? "mpi"
+    args << "--enable-openmp" if build.with? "openmp"
+
+    ENV.universal_binary if build.universal?
+
+    # single precision
+    # enable-sse2 and enable-avx works for both single and double precision
+    system "./configure", "--enable-single", "--enable-type-prefix", *(args + simd_args)
+    system "make", "install"
+
+    # clean up so we can compile the double precision variant
+    system "make", "clean"
+
+    # double precision
+    # enable-sse2 and enable-avx works for both single and double precision
+    system "./configure", "--enable-type-prefix", *(args + simd_args)
+    system "make", "install"
+
+    # clean up so we can compile the double version w/o type prefixes
+    system "make", "clean"
+
+    # double precision
+    # enable-sse2 and enable-avx works for both single and double precision
+    system "./configure", *(args + simd_args)
+    system "make", "install"
+
+  end
+end
