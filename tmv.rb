4044b04cd69ebe80b02d12d727c8cfc42b7016f6
Matthew R. Becker <beckermr@users.noreply.github.com>
2015-11-07 22:36:13 -0800

    Update tmv.rb

--- tmv.rb
+++ tmv.rb
@@ -4,11 +4,11 @@ class Tmv < Formula
   url "https://github.com/rmjarvis/tmv/archive/v0.72.tar.gz"
   sha256 "39083c804fc1015a08e1e414bb47764f6ea906c06103f0dea3555b734ad57430"
   
-  depends_on "scons"
+  depends_on "scons" => :build
   
   def install
-    system "scons", "PREFIX=#{prefix}", "WITH_BLAS=false"
-    system "scons", "install"
+    scons "PREFIX=#{prefix}", "WITH_BLAS=false"
+    scons "install"
   end
 end
   
