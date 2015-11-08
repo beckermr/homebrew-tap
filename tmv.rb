beade9c78ac5a5b8ddecc72f2f5fafb3a6869aed
Matthew R. Becker <beckermr@users.noreply.github.com>
2015-11-07 22:37:14 -0800

    Update tmv.rb

--- tmv.rb
+++ tmv.rb
@@ -3,6 +3,7 @@ class Tmv < Formula
   homepage "https://github.com/rmjarvis/tmv"
   url "https://github.com/rmjarvis/tmv/archive/v0.72.tar.gz"
   sha256 "39083c804fc1015a08e1e414bb47764f6ea906c06103f0dea3555b734ad57430"
+  head "https://github.com/rmjarvis/tmv"
   
   depends_on "scons" => :build
   
