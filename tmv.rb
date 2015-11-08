84e8cdd68581d2bdc4826e8d9ea68377f7f649fd
Matthew R Becker <becker.mr@gmail.com>
2015-11-07 23:39:58 -0800

    compiles now

--- tmv.rb
+++ tmv.rb
@@ -1,12 +1,14 @@
 class Tmv < Formula
   desc "TMV C++ linear algebra library"
   homepage "https://github.com/rmjarvis/tmv"
-  url "https://googledrive.com/host/0B6hIz9tCW5iZdEcybFNjRHFmOEE/tmv0.72.tar.gz"
+  url "https://github.com/rmjarvis/tmv/archive/v0.72.tar.gz"
   sha256 "39083c804fc1015a08e1e414bb47764f6ea906c06103f0dea3555b734ad57430"
   
   depends_on "scons" => :build
   
   def install
+    # hack the TMV version script to just echo the current version
+    inreplace "#{buildpath}/bin/tmv-version", "#!/bin/bash", "#!/bin/bash\necho 0.72\nexit"
     scons "PREFIX=#{prefix}", "WITH_BLAS=false"
     scons "install"
   end
